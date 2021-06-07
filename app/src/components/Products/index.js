838ef3fd2a97f9d7d060483a4ca1d9fd917663d9
Renan Paixão <renanlemos21@gmail.com>
2021-06-06 23:14:21 -0300

    feat(search): adicionada funcionalidade de search

--- app/src/components/Products/index.js
+++ app/src/components/Products/index.js
@@ -6,25 +6,23 @@ import Search from '../Search';
 
 function Products() {
 	const [cards, setCards] = useState([]);
+
 	useEffect(() => {
-		let isMounted = true;
+		list();
+	}, []);
+	function list() {
 		// put the port that you using here
 		axios.get(`http://localhost:404/products`).then((res) => {
-			if (isMounted) {
-				setCards(res.data);
-			}
+			setCards(res.data);
 		});
-		return () => {
-			isMounted = false;
-		};
-	}, [cards]);
+	}
 
 	return (
 		<>
-			<Search />
+			<Search cards={cards} setCards={setCards} list={list} />
 			<WrapperCards>
 				{cards === false || cards.length === 0 ? (
-					<h1>Não temos produtos ou você não está conectado a internet</h1>
+					<h1>Não temos produtos</h1>
 				) : (
 					cards.map((props) => {
 						return (
--- app/src/components/Search/index.js
+++ app/src/components/Search/index.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import SearchStyle from './style.js';
 
-function Search() {
+function Search({ cards, setCards, list }) {
 	return (
 		<SearchStyle>
-			<input id="search" type="text" placeholder="Insira o nome do produto"></input>
-			<input id="buttonSearch" type="submit" value="Procurar"></input>
+			<input
+				id="search"
+				type="text"
+				placeholder="Insira o nome do produto"
+				onChange={() => {
+					if (document.getElementById('search').value === '') {
+						list();
+					}
+				}}
+			></input>
+			<input
+				id="buttonSearch"
+				type="submit"
+				value="Procurar"
+				onClick={() => {
+					const value = document.getElementById('search').value;
+					let newCards;
+
+					newCards = cards.filter((card) => {
+						if (card.title.toLowerCase() === value.toLowerCase()) {
+							return card;
+						}
+					});
+
+					if (newCards.length > 0) {
+						setCards(newCards);
+					} else {
+						alert('produto não encontrado!');
+					}
+				}}
+			></input>
 		</SearchStyle>
 	);
 }
